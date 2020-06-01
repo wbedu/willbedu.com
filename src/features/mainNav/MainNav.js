f1c183e01ddc4e945ca33080069428505242e5c9
user@example.com <user@example.com>
2020-05-31 23:35:53 -0500

    edit navbar

--- src/features/mainNav/MainNav.js
+++ src/features/mainNav/MainNav.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap'
 import { faIdCard, faCode, faBlog, faCodeBranch } from '@fortawesome/free-solid-svg-icons'
 import NavLink from './navLink/navLink'
-import './mainNav.css'
 import  "hover.css"
 
 export default function MainNav() {
@@ -28,25 +27,9 @@ export default function MainNav() {
                 <div className="brand-link"><p>WILL</p><p>BEDU</p></div>
                 <NavLink href="/" icon={faIdCard} size="lg" text="Home" />
                 <div
-                    style={{
-                        borderBottomColor: 'black',
-                        borderBottomWidth: 1,
-                    }}
                 />
                 <NavLink href="/projects" icon={faCode} size="lg" text="Projects" />
-                <div
-                    style={{
-                        borderBottomColor: 'black',
-                        borderBottomWidth: 1,
-                    }}
-                />
                 <NavLink href="/blog" icon={faBlog} size="lg" text="Blog" />
-                <div
-                    style={{
-                        borderBottomColor: 'black',
-                        borderBottomWidth: 1,
-                    }}
-                />
                 <NavLink href="/resume" icon={faCodeBranch} size="lg" text="Resume" />
             </Nav>
         </div>
--- src/features/mainNav/navLink/navLink.js
+++ src/features/mainNav/navLink/navLink.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Nav } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import "./navlink.css"
+
 export default function NavLink(props) {
     return (
-        <Nav.Link className={"hvr-forward"} href={props.href}>
-            <FontAwesomeIcon icon={props.icon} size={props.size||"lg"} />
-            <p >{props.text}</p>
+        <div className={"c-nav-link"} >
+        <Nav.Link className={"hvr-forward c-nav-link"} href={props.href}>
+            <FontAwesomeIcon className={"nav-link-icon"} icon={props.icon} size={props.size||"lg"} />
+            <p className={"nav-link-text center"}>{props.text}</p>
         </Nav.Link>
+        </div>
     )
 }
