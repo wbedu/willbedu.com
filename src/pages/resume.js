c73a4f4c6a94158f6cc1b237761b1a1d1bc7091d
wbedu <wbedu@localhost.localdomain>
2020-06-11 22:02:08 -0500

    projects placeholder

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,11 +1,53 @@
 import React from 'react';
 import '../styles/defaultPageStyle.css'
+import  './resume.css'
+import {Card} from "react-bootstrap";
 export default function Resume() {
     document.title = "WB| Resume"
     return (
         <main>
             <h1>Resume</h1>
-            There is nothing here yet
+            <p>
+                Working on this page. Check back soon
+            </p>
+            {/*<div className="c-timeline">*/}
+            {/*    <Card className="c-timeline-card">*/}
+            {/*        <div className="content">*/}
+            {/*            <h2>2017</h2>*/}
+            {/*            <p>Lorem ipsum..</p>*/}
+            {/*        </div>*/}
+            {/*    </Card>*/}
+            {/*    <Card className="c-timeline-card">*/}
+            {/*        <div className="content">*/}
+            {/*            <h2>2016</h2>*/}
+            {/*            <p>Lorem ipsum..</p>*/}
+            {/*        </div>*/}
+            {/*    </Card>*/}
+            {/*    <Card className="c-timeline-card">*/}
+            {/*        <div className="content">*/}
+            {/*            <h2>2017</h2>*/}
+            {/*            <p>Lorem ipsum..</p>*/}
+            {/*        </div>*/}
+            {/*    </Card>*/}
+            {/*    <Card className="c-timeline-card">*/}
+            {/*        <div className="content">*/}
+            {/*            <h2>2016</h2>*/}
+            {/*            <p>Lorem ipsum..</p>*/}
+            {/*        </div>*/}
+            {/*    </Card>*/}
+            {/*    <Card className="c-timeline-card">*/}
+            {/*        <div className="content">*/}
+            {/*            <h2>2017</h2>*/}
+            {/*            <p>Lorem ipsum..</p>*/}
+            {/*        </div>*/}
+            {/*    </Card>*/}
+            {/*    <Card className="c-timeline-card">*/}
+            {/*        <div className="content">*/}
+            {/*            <h2>2016</h2>*/}
+            {/*            <p>Lorem ipsum..</p>*/}
+            {/*        </div>*/}
+            {/*    </Card>*/}
+            {/*</div>*/}
         </main>
     )
 }
