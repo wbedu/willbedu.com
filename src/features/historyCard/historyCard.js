54cee1eead524e67e696f8d4fb8b581735c8d25f
wbedu <wbedu@localhost.localdomain>
2020-06-26 20:46:30 -0500

    remove test content

---
+++ src/features/historyCard/historyCard.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Card} from 'react-bootstrap'
+
+export default function HistoryCard(props) {
+    return (
+        <Card data-aos="slide-up"
+              className="col-3 c-bg-secondary c-text-primary c-history-card c-timeline-card col-3">
+            <Card.Title className={`center c-history-card-title c-history-card-title-${props.historyType} c-bg-tertiary`}>
+                <div class={"c-history-role-title-container"}>
+                    <p className={"c-history-role"}>
+                        {props.role.name}
+                    </p>
+                    <p className={"c-history-role-location"}>
+                        - {props.role.location}
+                    </p>
+                </div>
+            </Card.Title>
+            <Card.Body className={"c-gh-card-body content"}>
+                <div className={"text-left-align"}>
+                    <p>{props.description}</p>
+                    <ul>
+                        {props.workItems.map((work, index) => {
+                            return (
+                                <li key={`work-${props.name}-${index}`}>{work}</li>
+                            )
+                        })}
+                    </ul>
+                </div>
+            </Card.Body>
+        </Card>
+    )
+}
--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,53 +1,77 @@
 import React from 'react';
 import '../styles/defaultPageStyle.css'
-import  './resume.css'
-// import {Card} from "react-bootstrap";
+import './resume.css'
+import {Card} from "react-bootstrap";
+import HistoryCard from "../features/historyCard/historyCard";
+
 export default function Resume() {
     document.title = "WB| Resume"
+
+
+    const mockHistory = [
+        {
+            role: {
+                name: "role1",
+                location: "company1"
+            },
+            historyType: "type2",
+            description: "I did stuff",
+            workItems: ["stuff1", "stuff2", "stuff3", "stuff4"]
+
+        },
+        {
+            role: {
+                name: "role1",
+                location: "company1"
+            },
+            historyType: "type2",
+            description: "I did stuff",
+            workItems: ["stuff1", "stuff2", "stuff3", "stuff4"]
+
+        },
+        {
+            role: {
+                name: "role1",
+                location: "company1"
+            },
+            historyType: "type2",
+            description: "I did stuff",
+            workItems: ["stuff1", "stuff2", "stuff3", "stuff4"]
+
+        },
+        {
+            role: {
+                name: "role1",
+                location: "company1"
+            },
+            historyType: "type2",
+            description: "I did stuff",
+            workItems: ["stuff1", "stuff2", "stuff3", "stuff4"]
+
+        }
+    ]
+
     return (
         <main>
             <h1>Resume</h1>
             <p>
                 Working on this page. Check back soon
             </p>
-            {/*<div className="c-timeline">*/}
-            {/*    <Card className="c-timeline-card">*/}
-            {/*        <div className="content">*/}
-            {/*            <h2>2017</h2>*/}
-            {/*            <p>Lorem ipsum..</p>*/}
-            {/*        </div>*/}
-            {/*    </Card>*/}
-            {/*    <Card className="c-timeline-card">*/}
-            {/*        <div className="content">*/}
-            {/*            <h2>2016</h2>*/}
-            {/*            <p>Lorem ipsum..</p>*/}
-            {/*        </div>*/}
-            {/*    </Card>*/}
-            {/*    <Card className="c-timeline-card">*/}
-            {/*        <div className="content">*/}
-            {/*            <h2>2017</h2>*/}
-            {/*            <p>Lorem ipsum..</p>*/}
-            {/*        </div>*/}
-            {/*    </Card>*/}
-            {/*    <Card className="c-timeline-card">*/}
-            {/*        <div className="content">*/}
-            {/*            <h2>2016</h2>*/}
-            {/*            <p>Lorem ipsum..</p>*/}
-            {/*        </div>*/}
-            {/*    </Card>*/}
-            {/*    <Card className="c-timeline-card">*/}
-            {/*        <div className="content">*/}
-            {/*            <h2>2017</h2>*/}
-            {/*            <p>Lorem ipsum..</p>*/}
-            {/*        </div>*/}
-            {/*    </Card>*/}
-            {/*    <Card className="c-timeline-card">*/}
-            {/*        <div className="content">*/}
-            {/*            <h2>2016</h2>*/}
-            {/*            <p>Lorem ipsum..</p>*/}
-            {/*        </div>*/}
-            {/*    </Card>*/}
-            {/*</div>*/}
+            <div className="c-timeline col-12">
+                {/*{
+                    mockHistory.map((history, index) => {
+                        return (
+                            <HistoryCard{...history}> </HistoryCard>
+                        )
+                    })
+                }
+                <Card className="c-timeline-card col-3">
+                    <div className="content">
+                        <h2>2017</h2>
+                        <p>Lorem ipsum..</p>
+                    </div>
+                </Card>*/}
+            </div>
         </main>
     )
 }
